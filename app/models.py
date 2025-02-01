dff6926c0e1d8613c357ac439ddd255cb7eabd56
pandu sai <saiguptha_v@srmap.edu.in>
2025-02-01 10:37:27 +0530

    fix: Modified the model with translate class method

--- app/models.py
+++ app/models.py
@@ -1,19 +1,72 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_ckeditor import CKEditorField
 from sqlalchemy import Column, Integer, Text
-
+from flask import current_app as app, json
+import requests
+import json
+import logging
+from .config import Config
+from .translations import sign_request
 db = SQLAlchemy()
+AWS_ACCESS_KEY = Config.AWS_ACCESS_KEY
+AWS_SECRET_KEY = Config.AWS_SECRET_KEY
+AWS_REGION = Config.AWS_REGION
+SERVICE = Config.SERVICE
+API_URL = Config.API_URL
 
 class FAQ(db.Model):
     __tablename__ = 'faqs'
 
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String(255), nullable=False)
-    answer = db.Column(db.Text, nullable=False)  # Use Text for database storage
+    answer = db.Column(db.Text, nullable=False)
 
     def __init__(self, question, answer, question_hi=None, question_bn=None):
         self.question = question
         self.answer = answer
     def get_question(self):
-        """Method to retrieve the question in English or translated."""
+
         return self.question
+        
+    def translate_faq(self, lang):
+            payload = json.dumps({
+                "question": self.question,
+                "answer": self.answer,
+                "target_language": lang
+            })
+
+            signed_headers = sign_request("POST", API_URL, payload, AWS_ACCESS_KEY, AWS_SECRET_KEY, AWS_REGION, SERVICE)
+
+            try:
+                response_faq = requests.post(API_URL, data=payload, headers=signed_headers)
+                app.logger.info(f"API Response Code: {response_faq.status_code}")
+
+                if response_faq.status_code == 200:
+                    response_data = response_faq.json()
+                    response_data = json.loads(response_data["body"])
+                    translated_question = response_data.get("translated_text_question", {}).get("TranslatedText", self.question)
+                    translated_answer = response_data.get("translated_text_answer", {}).get("TranslatedText", self.answer)
+
+                    app.logger.info(f"Translated Question: {translated_question}")
+                    app.logger.info(f"Translated Answer: {translated_answer}")
+
+                    return {
+                        "id": self.id,
+                        "question": translated_question,
+                        "answer": translated_answer
+                    }
+                else:
+                    return {
+                        "question": self.question,
+                        "answer": self.answer,
+                        "response": response_faq.text
+                    }
+
+            except requests.exceptions.RequestException as e:
+                app.logger.error(f"Request failed: {e}")
+
+                return {
+                    "question": self.question,
+                    "answer": self.answer,
+                    "response": str(e)
+                }
